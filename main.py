404e28002cd215bda424ff126a5f9601db1e15b8
TetoDola <sudaravan10@icloud.com>
2025-02-18 12:02:49 +0100

    Moves based on User move and FEN. (not yet API)

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
+import os
+
 import chess
 import chess.engine
-#bruhss
-engine = chess.engine.SimpleEngine.popen_uci(r"C:\Users\Teto\Downloads\stockfish-windows-x86-64-avx2 (1)\stockfish\stockfish-windows-x86-64-avx2.exe")
-board = chess.Board()
-while not board.is_game_over():
-    print(board)
-    if board.turn == chess.WHITE:
-        print("White's turn")
-        try:
-            move = chess.Move.from_uci(input("Enter your move: "))
-            if move in board.legal_moves:
-                board.push(move)
-        except Exception as e:
-            print(e)
-            continue
-    else:
-        print("Black's turn")
-    result = engine.play(board, chess.engine.Limit(time=0.1))
-    print(result)
-    board.push(result.move)
-    print(board)
+import os
+from dotenv import load_dotenv
 
-engine.quit()
+load_dotenv()
+
+#bruhss
+engine = chess.engine.SimpleEngine.popen_uci(os.getenv('CHESSENGINE_PATH'))
+
+def initialize_board():
+    board = chess.Board()
+    fen = input("Enter the FEN: ")
+    user_move = input("Enter your move: ")
+
+    try:
+        board = chess.Board(fen)
+        print(board)
+        print(board.turn)
+    except Exception as error:
+        print(error)
+
+    try:
+        move = chess.Move.from_uci(user_move)
+        if move in board.legal_moves:
+            board.push(move)
+            print(board)
+            return board
+        else:
+            print("Invalid move")
+            initialize_board()
+    except Exception as error:
+        print(error)
+
+def main():
+    b = initialize_board()
+    result = engine.play(b, chess.engine.Limit(time=0.1))
+    print(f"Engine move: {result.move}")
+
+if __name__ == "__main__":
+    main()
